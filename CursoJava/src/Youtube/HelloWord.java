905d41c74da740d855b99e655c457904ab81b17b
rodrigooliveira <rodrigooliveira@192.168.15.5>
2022-03-28 20:35:55 -0300

    Testando o git hub - Adicionado a class HelloWord.

---
+++ CursoJava/src/Youtube/HelloWord.java
@@ -0,0 +1,11 @@
+package Youtube;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word!!");
+		System.out.println("Projeto Clonado do GitHub!!!");
+	}
+
+}
