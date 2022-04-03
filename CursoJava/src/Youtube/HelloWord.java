a85ae07da1626fa0a633f04860568ef8a8af0783
rodrigolvsantos <rodrigo.olv.santos@gmail.com>
2022-04-03 09:53:26 -0300

    Teste Samsung Windows - Add line to hwlloword

--- CursoJava/src/Youtube/HelloWord.java
+++ CursoJava/src/Youtube/HelloWord.java
@@ -7,6 +7,7 @@ public class HelloWord {
 		System.out.println("Hello Word!!");
 		System.out.println("Projeto Clonado do GitHub!!!");
 		System.out.println("Teste macbook pro!!!");
+		System.out.println("Teste Samsung Windows!!!");
 	}
 
 }
